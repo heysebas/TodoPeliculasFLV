522964872eebf9ea921959f0fd42e81790d53a9f
heysebas <sebazgrisales@gmail.com>
2023-03-10 23:28:05 -0500

    nuevas_peli_mi_padre

--- js/header.js
+++ js/header.js
@@ -3,184 +3,230 @@ header.innerHTML = `
 
 <a href="/index.html"><img class="log" src="/images/logo2.png" alt="logo"></a>
 <nav>
-    <ul class="nav_links">
-        <li> <a href="/index.html">Inicio</a></li>
-        <li> <a href="/menu/peliculas.html">Peliculas</a></li>
-        <li> <a href="/menu/series.html">Series</a></li>
-    </ul>
+<ul class="nav_links">
+    <li> <a href="/index.html">Inicio</a></li>
+    <li> <a href="/menu/peliculas.html">Peliculas</a></li>
+    <li> <a href="/menu/series.html">Series</a></li>
+</ul>
 
 </nav>
 <a class="cta" href="https://t.me/+-4vHUMv4sN44Yjdh" target="_blank"><button class="bonto-1">Contacto</button></a>
 <form id="form">
-    <input type="search" placeholder="Buscar" id="input-search" class="search">
+<input type="search" placeholder="Buscar" id="input-search" class="search">
 </form>
 <div class="bx bx-menu" id="menu-icon"> <img src="/images/menu-line.png" alt=""></div>
 <div id="content-footer">
-    <div id="lbl-footer" class="content-search">
-        <div class="content-table">
-            <table id="table">
-                <thead>
+<div id="lbl-footer" class="content-search">
+    <div class="content-table">
+        <table id="table">
+            <thead>
+                <tr>
+                    <td></td>
+                </tr>
+            </thead>
+            <tbody>
+
+
+
                     <tr>
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
+                    <td>
+                        <a href="/peliculas/Mi-Padre.html">
+                            <div>Mi padre</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Tenemos-un-fantasma.html">
+                            <div>Tenemos un fantasma</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Duro-de-entrenar.html">
+                            <div>Duro de entrenar</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Punto-de-fuga.html">
+                            <div>Punto de fuga</div>
+                        </a>
+                    </td>
+                </tr>            
+                <tr>
+                    <td>
+                        <a href="/peliculas/El-abismo.html">
+                            <div>El abismo</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Esta-noche-duermes-conmigo.html">
+                            <div>Esta noche duermes conmigo</div>
+                        </a>
+                    </td>
+                </tr>
 
                 <tr>
-                <td>
-                    <a href="/peliculas/Princess-Khutulun.html">
-                       <div>Princess Khutulun</div>
-                   </a>
-                </td>
-            </tr>
-           <tr>
-           <td>
-               <a href="/peliculas/Jack-in-the-Box-2-El-despertar.html">
-                   <div>Jack in the Box 2: El despertar</div>
-               </a>
-           </td>
-       </tr>
-       <tr>
-           <td>
-               <a href="/peliculas/Jack-in-the-Box-2-El-despertar.html">
-                   <div>The Jack in the Box: Awakening</div>
-               </a>
-           </td>
-           </tr>
+                    <td>
+                        <a href="/peliculas/Princess-Khutulun.html">
+                            <div>Princess Khutulun</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Jack-in-the-Box-2-El-despertar.html">
+                            <div>Jack in the Box 2: El despertar</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Jack-in-the-Box-2-El-despertar.html">
+                            <div>The Jack in the Box: Awakening</div>
+                        </a>
+                    </td>
+                               
+                </tr>
 
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Arde-Notre-Dame.html">
-                                <div>Arde Notre Dame</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Arde-Notre-Dame.html">
-                                <div>Notre-Dame On Fire</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Huesera.html">
-                                <div>Huesera</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Identidad-desbloqueda.html">
-                                <div>Identidad desbloqueada</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/El-ultimo-baile-de-Magic-Mike.html">
-                                <div>El último baile de Magic Mike</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/El-ultimo-baile-de-Magic-Mike.html">
-                                <div>Magic Mike's Last Dance</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Sharper-Un-plan-perfecto.html">
-                                <div>Sharper: Un plan perfecto</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Un-vecino-grunon.html">
-                                <div>Un vecino gruñón</div>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Un-vecino-grunon.html">
-                                <div> A Man Called Otto</div>
-                            </a>
-                        </td>
-                    </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Arde-Notre-Dame.html">
+                            <div>Arde Notre Dame</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Arde-Notre-Dame.html">
+                            <div>Notre-Dame On Fire</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Huesera.html">
+                            <div>Huesera</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Identidad-desbloqueda.html">
+                            <div>Identidad desbloqueada</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/El-ultimo-baile-de-Magic-Mike.html">
+                            <div>El último baile de Magic Mike</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/El-ultimo-baile-de-Magic-Mike.html">
+                            <div>Magic Mike's Last Dance</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Sharper-Un-plan-perfecto.html">
+                            <div>Sharper: Un plan perfecto</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Un-vecino-grunon.html">
+                            <div>Un vecino gruñón</div>
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Un-vecino-grunon.html">
+                            <div> A Man Called Otto</div>
+                        </a>
+                    </td>
+                </tr>
 
-                    <tr>
+                <tr>
 
-                        <td>
-                            <a href="/peliculas/Pantera-Negra-Wakanda-por-siempre.html">
-                                <div>Pantera Negra: Wakanda por siempre</div>
-                            </a>
-                        </td>
+                    <td>
+                        <a href="/peliculas/Pantera-Negra-Wakanda-por-siempre.html">
+                            <div>Pantera Negra: Wakanda por siempre</div>
+                        </a>
+                    </td>
 
-                    </tr>
-                    <tr>
+                </tr>
+                <tr>
 
-                        <td>
-                            <a href="/peliculas/Pantera-Negra-Wakanda-por-siempre.html">
-                                <div>Black Panther: Wakanda Forever</div>
-                            </a>
-                        </td>
+                    <td>
+                        <a href="/peliculas/Pantera-Negra-Wakanda-por-siempre.html">
+                            <div>Black Panther: Wakanda Forever</div>
+                        </a>
+                    </td>
 
-                    </tr>
+                </tr>
 
-                    <tr>
+                <tr>
 
-                        <td>
-                            <a href="/Series/Peacemaker/Peacemaker.html">
-                                <div>Peacemaker</div>
-                            </a>
-                        </td>
+                    <td>
+                        <a href="/Series/Peacemaker/Peacemaker.html">
+                            <div>Peacemaker</div>
+                        </a>
+                    </td>
 
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/peliculas/Encanto.html">
-                                <div>Encanto</div>
-                            </a>
-                        </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/peliculas/Encanto.html">
+                            <div>Encanto</div>
+                        </a>
+                    </td>
 
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/Series/Hawkeye/Hawkeye.html">
-                                <div>Hawkeye</div>
-                            </a>
-                        </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/Series/Hawkeye/Hawkeye.html">
+                            <div>Hawkeye</div>
+                        </a>
+                    </td>
 
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/Series/Falcon-Y-El-Soldado-Del-Invierno/Falcon-Y-El-Soldado-Del-Invierno.html">
-                                <div>Falcon Y El Soldado Del Invierno</div>
-                            </a>
-                        </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/Series/Falcon-Y-El-Soldado-Del-Invierno/Falcon-Y-El-Soldado-Del-Invierno.html">
+                            <div>Falcon Y El Soldado Del Invierno</div>
+                        </a>
+                    </td>
 
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/Series/Falcon-Y-El-Soldado-Del-Invierno/Falcon-Y-El-Soldado-Del-Invierno.html">
-                                <div>The Falcon and the Winter Soldier</div>
-                            </a>
-                        </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="/Series/Falcon-Y-El-Soldado-Del-Invierno/Falcon-Y-El-Soldado-Del-Invierno.html">
+                            <div>The Falcon and the Winter Soldier</div>
+                        </a>
+                    </td>
 
-                    </tr>
-
-
-
-                </tbody>
-            </table>
-        </div>
+                </tr>
 
 
+
+            </tbody>
+        </table>
     </div>
+
+
+</div>
 
 </div>
 
@@ -198,3 +244,41 @@ menu.onclick = () => {
 menu.classList.toggle('.bx-menu');
 navbar.classList.toggle('open');
 }
+
+$(document).ready(function(){
+
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+
+	// FILTRANDO PRODUCTOS  ============================================
+
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+});
