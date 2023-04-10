2a11b2222d276188d63cd6f2fcb5b9b153d0edee
heysebas <sebazgrisales@gmail.com>
2023-04-09 22:58:33 -0500

    pagination

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+
+
 // const header = document.querySelector("header");
+
+
+
 const footer = document.querySelector("footer");
 inputSearch = document.getElementById("inputSearch");
 box_search = document.getElementById("box-search");
@@ -30,90 +35,136 @@ footer.innerHTML = `
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
-	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+    // AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+    $('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-	// FILTRANDO PRODUCTOS  ============================================
+    // FILTRANDO PRODUCTOS  ============================================
 
-	$('.category_item').click(function(){
-		var catProduct = $(this).attr('category');
-		console.log(catProduct);
+    $('.category_item').click(function () {
+        var catProduct = $(this).attr('category');
+        console.log(catProduct);
 
-		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
-		$('.category_item').removeClass('ct_item-active');
-		$(this).addClass('ct_item-active');
+        // AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+        $('.category_item').removeClass('ct_item-active');
+        $(this).addClass('ct_item-active');
 
-		// OCULTANDO PRODUCTOS =========================
-		$('.product-item').css('transform', 'scale(0)');
-		function hideProduct(){
-			$('.product-item').hide();
-		} setTimeout(hideProduct,400);
+        // OCULTANDO PRODUCTOS =========================
+        $('.product-item').css('transform', 'scale(0)');
+        function hideProduct() {
+            $('.product-item').hide();
+        } setTimeout(hideProduct, 400);
 
-		// MOSTRANDO PRODUCTOS =========================
-		function showProduct(){
-			$('.product-item[category="'+catProduct+'"]').show();
-			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
-		} setTimeout(showProduct,400);
-	});
+        // MOSTRANDO PRODUCTOS =========================
+        function showProduct() {
+            $('.product-item[category="' + catProduct + '"]').show();
+            $('.product-item[category="' + catProduct + '"]').css('transform', 'scale(1)');
+        } setTimeout(showProduct, 400);
+    });
 
-	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+    // MOSTRANDO TODOS LOS PRODUCTOS =======================
 
-	$('.category_item[category="all"]').click(function(){
-		function showAll(){
-			$('.product-item').show();
-			$('.product-item').css('transform', 'scale(1)');
-		} setTimeout(showAll,400);
-	});
+    $('.category_item[category="all"]').click(function () {
+        function showAll() {
+            $('.product-item').show();
+            $('.product-item').css('transform', 'scale(1)');
+        } setTimeout(showAll, 400);
+    });
 });
 
 
 
+//--------------------------------------------------------------------//
 
 
+function getPageList(totalPages, page, maxLength) {
+    function range(start, end) {
+        return Array.from(Array(end - start + 1), (_, i) => i + start);
+    }
+    var sideWidth = maxLength < 9 ? 1 : 2;
+    var lefWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+    var rigtWidth = (maxLength - sideWidth * 2 - 3) >> 1;
 
-const ul = document.querySelector('ul');
-let allPages = 15;
-
-function elem(allPages, page){
-    let li = '';
-
-    let beforePages = page - 1;
-    let afterPages = page + 1;
-    let liActive;
-
-    if(page > 1){
-        li += `<li class="btn" onclick="elem(allPages, ${page-1})" ><i class="fas fa-angle-left"></i></li>`;
+    if (totalPages <= maxLength) {
+        return range(1, totalPages);
     }
 
-    for (let pageLength = beforePages; pageLength <= afterPages; pageLength++){
-
-        if(pageLength > allPages){
-            continue;
-        }
-        if(pageLength == 0){
-            pageLength = pageLength + 1;
-        }
-
-        if(page == pageLength){
-            liActive = 'active';
-        }else{
-            liActive = '';
-        }
-
-        li += `<li class="numb ${liActive}" onclick="elem(allPages, ${pageLength})" ><span>${pageLength}</span></li>`
+    if (page <= maxLength - sideWidth - 1 - rigtWidth) {
+        return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
     }
 
-    if(page < allPages){
-        li += `<li class="btn" onclick="elem(allPages, ${page+1})" ><i class="fas fa-angle-right"></i></li>`;
+    if (page >= totalPages - sideWidth - 1 - rigtWidth) {
+        return renge(1, sideWidth).concat(0, range(totalPages- sideWidth - 1 - rigtWidth - lefWidth, totalPages));
     }
 
-    ul.innerHTML = li;
+    return range(1, sideWidth).concat(0, range(page - lefWidth, page + rigtWidth), 0, range(totalPages - sideWidth + 1, totalPages));
 }
-elem(allPages, 2);
+
+// $(function () {
+//     var numberOfItems = $(".card-content .movie").length;
+//     var limitPerPage = 32;
+//     var totalPages = Math.ceil(numberOfItems / limitPerPage);
+//     var paginationSize = 2
+//     var currentPage;
+
+//     function showPage(whichPage) {
+//         if (whichPage < 1 || whichPage > totalPages) return false;
+
+//         currentPage = whichPage;
+
+//         $(".card-content .movie").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+//         // $(".pagination li").slice(1, -1).remove();
+
+//         getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+//             $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
+//                 .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
+//                     .attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
+//         });
+
+//         $(".previous-page").toggleClass("disable", currentPage === 1);
+//         $(".next-page").toggleClass("disable", currentPage === totalPages);
+//         return true;
+//     }
+
+//     $(".pagination").append(
+//         // $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text("Prev")),
+//         // $("<li>").addClass("page-item").addClass("netx-page").append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text("Next"))
+//     );
+
+//     $(".card-content").show();
+//     showPage(1);
+
+//     $(document).on("click", ".pagination li.current-page:not(.active)", function () {
+//         return showPage(+$(this).text());
+//     });
+
+//     $(".next-page").on("click", function () {
+//         return showPage(currentPage + 1);
+//     });
+
+//     $(".previous-page").on("click", function () {
+//         return showPage(currentPage - 1);
+//     });
+// });
 
 
+var items = $('.card-content .movie');
+var  numItems = items.length;
+var perPage = 30; //cantida de cards mostrados
 
+items.slice(perPage).hide();
 
-
+$('#pagination-container').pagination({
+    items: numItems,
+    itemsOnPage : perPage,
+    prevText: "Prev",
+    netxText: "Next",
+    onPageClick: function (pageNumber) {
+        var showFrom = perPage * (pageNumber - 1);
+        var showTo = showFrom + perPage;
+        items.hide(),slice(showFrom, showTo).show();
+    
+    }
+});
