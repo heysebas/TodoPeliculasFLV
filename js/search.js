c132c7426749e8d06332f9c925b06013da61a035
sebastian <sebazgrisales@gmail.com>
2023-09-22 10:35:43 -0500

    Arreglado js de search a buscar con acentos

--- js/search.js
+++ js/search.js
@@ -1,16 +1,25 @@
 var buscador = $("#table").DataTable();
 
-$("#input-search").keyup(function(){
-    
-    buscador.search($(this).val()).draw();
-    
-    if ($("#input-search").val() == ""){
+// Función para normalizar una cadena de texto eliminando acentos
+function normalizeString(input) {
+    return input.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+$.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+    var searchTerm = normalizeString($("#input-search").val());
+
+    // Normalizar y eliminar acentos de la cadena en la fila de datos
+    var rowData = data.join(' ').toLowerCase();
+    rowData = normalizeString(rowData);
+
+    return rowData.includes(searchTerm);
+});
+
+$("#input-search").keyup(function () {
+    buscador.draw();
+    if ($(this).val() === "") {
         $(".content-search").fadeOut(300);
-    }else{
+    } else {
         $(".content-search").fadeIn(300);
     }
-})
-
-
-
-
+});
